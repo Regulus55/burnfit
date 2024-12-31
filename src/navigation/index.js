fa6ec4710726ce350c492c6089a5f90161f90071
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2024-12-31 13:19:09 +0900

    navi index screenOptions

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,14 +2,22 @@ import SCREENS from "../screens/tabs";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
 import { COLORS } from "../constants";
-
 import screens from "../commons/tabMenus";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator initialRouteName={SCREENS.HOME}>
+    <Tab.Navigator
+      initialRouteName={SCREENS.HOME}
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: 70,
+          // paddingBottom: 1,
+        },
+      }}
+    >
       {screens.map(({ name, component, icon }) => (
         <Tab.Screen
           key={name}
